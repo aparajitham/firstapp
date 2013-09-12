e75b82580f9c0d4dc6448b7ae7e211e25fd53ab0
aparajitham <jeetu.vii@gmail.com>
2013-09-12 17:54:22 +0530

    foldsers

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Firstapp::Application.config.secret_token = 'ebaaf3402cdb948421829c01d727d1603f8382827d3969d1fc20a079627a78c8d1d3f3f30558ada7a51d5324343c88ff0f7ae594d658ea549c2d0da7dd9eca41'
